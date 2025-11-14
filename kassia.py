0f4ba94789c7929821bb0c1dda45a5926d8a56a1
ai-tonchev <147974544+ai-tonchev@users.noreply.github.com>
2025-11-14 13:51:42 +0200

    stable-ish
    recreated error from main

--- kassia.py
+++ kassia.py
@@ -11,16 +11,16 @@ from reportlab.lib.styles import (ParagraphStyle, StyleSheet1,
 from reportlab.pdfbase import pdfmetrics
 from reportlab.platypus import PageBreak, Paragraph, Spacer
 
-from .complex_doc_template import ComplexDocTemplate
-from .coord import Coord
-from .drop_cap import Dropcap
-from .font_reader import find_and_register_fonts
-from .lyric import Lyric
-from .neume import Neume, NeumeBnml, NeumeType
-from .neume_chunk import NeumeChunk
-from .score import Score
-from .syllable import Syllable
-from .syllable_line import SyllableLine
+from builder.complex_doc_template import ComplexDocTemplate
+from builder.coord import Coord
+from builder.drop_cap import Dropcap
+from builder.font_reader import find_and_register_fonts
+from builder.lyric import Lyric
+from builder.neume import Neume, NeumeBnml, NeumeType
+from builder.neume_chunk import NeumeChunk
+from builder.score import Score
+from builder.syllable import Syllable
+from builder.syllable_line import SyllableLine
 
 
 class Kassia:
